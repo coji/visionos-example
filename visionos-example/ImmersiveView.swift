677e76fa7e31b85c54b35f5ddc5f77b58faa21ae
coji <coji@techtalk.jp>
2024-07-21 15:11:25 +0900

    Initial Commit

---
+++ visionos-example/ImmersiveView.swift
@@ -0,0 +1,25 @@
+//
+//  ImmersiveView.swift
+//  visionos-example
+//
+//  Created by coji on 2024/07/21.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(scene)
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .mixed) {
+    ImmersiveView()
+}
---
+++ visionos-example/visionos_exampleApp.swift
@@ -0,0 +1,21 @@
+//
+//  visionos_exampleApp.swift
+//  visionos-example
+//
+//  Created by coji on 2024/07/21.
+//
+
+import SwiftUI
+
+@main
+struct visionos_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
